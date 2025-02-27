f82f5f08f5a4aab5ce355952c6edada4d8dae67c
Visakh Vijayan <vjnvisakh@gmail.com>
2025-02-27 12:12:20 +0530

    finding video duration

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const electron = require('electron');
-const { app } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain, dialog } = electron;
 
 app.on('ready', () => {
-  console.log('App is ready');
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  ipcMain.handle('open-file', async () => {
+    const { canceled, filePaths } = await dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+        filters: [{ name: 'Videos', extensions: ['mp4', 'mkv', 'avi', 'mov'] }]
+    });
+
+    if (canceled) return null;
+
+    return filePaths[0]; // Return the selected video path
+  });
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log(metadata.format.duration);
+    });
 });
